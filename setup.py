a20332fbc6de97aa8e71c4149a781bd0a529340c
Daniel Watkins <daniel@daniel-watkins.co.uk>
2012-07-09 17:39:20 +0100

    Start again.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages,setup
 setup(
     name='sphinx-git',
     description='git Changelog for Sphinx',
-    version='3',
+    version='1',
     author='Daniel Watkins',
     author_email='daniel@daniel-watkins.co.uk',
     packages=find_packages(),
