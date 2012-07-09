e1e0a8a4a20fca3505f85dcdc1231c874d616662
Daniel Watkins <daniel@daniel-watkins.co.uk>
2012-07-09 17:37:48 +0100

    Fix requirements.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages,setup
 setup(
     name='sphinx-git',
     description='git Changelog for Sphinx',
-    version='1',
+    version='3',
     author='Daniel Watkins',
     author_email='daniel@daniel-watkins.co.uk',
     packages=find_packages(),
-    install_requires=['sphinx', 'PythonGit'],
+    install_requires=['sphinx', 'GitPython'],
     url="https://github.com/OddBloke/sphinx-git",
 )
