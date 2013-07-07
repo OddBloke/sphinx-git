7c0b84a6631dbf9945f76207cc1eaa5924fbfcdc
Daniel Watkins <daniel@daniel-watkins.co.uk>
2013-07-07 08:54:47 +0100

    Add sphinx_git to sample extensions.

--- sample/conf.py
+++ sample/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+sys.path = ['..'] + sys.path
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -25,7 +27,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx_git']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
