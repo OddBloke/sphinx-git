26a50e71e2b0d3b242cff92d28e84e905a9ba687
Daniel Watkins <daniel@daniel-watkins.co.uk>
2013-07-07 09:35:32 +0100

    Bump to v4 (for development).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='sphinx-git',
     description='git Changelog for Sphinx',
-    version='3',
+    version='4',
     author='Daniel Watkins',
     author_email='daniel@daniel-watkins.co.uk',
     install_requires=['sphinx', 'GitPython'],
