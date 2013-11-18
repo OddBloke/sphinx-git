10cef04202ea072e3fba5b1c00723f24bb59d73a
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-11-18 17:34:28 +0000

    Update copyright year.

--- sphinx_git.py
+++ sphinx_git.py
@@ -1,4 +1,4 @@
-# Copyright 2012 (C) Daniel Watkins <daniel@daniel-watkins.co.uk>
+# Copyright 2012-2013 (C) Daniel Watkins <daniel@daniel-watkins.co.uk>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
