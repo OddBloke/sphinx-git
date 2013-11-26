889ab33b3a343819c5feafebbf4fbdf146d18f1d
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-11-26 12:50:52 +0000

    Use execfile in setup.py to solve dependency chicken-egg problem.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 from sphinx_git import version
 
 
-version = version.__version__
+execfile('sphinx_git/version.py')
 
 
 setup(
     name='sphinx-git',
     description='git Changelog for Sphinx',
-    version=version,
+    version=__version__,
     author='Daniel Watkins',
     author_email='daniel@daniel-watkins.co.uk',
     install_requires=['sphinx', 'GitPython>=0.3.2.RC1'],
